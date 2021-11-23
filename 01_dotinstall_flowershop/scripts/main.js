5504535d4b08223274b1a9f2591f8c9716e9c71a
Shogo Ogami <sg.tao.so@gmail.com>
2021-11-24 08:20:54 +0900

    update 01_dotinsall for append popup style to products

--- 01_dotinstall_flowershop/scripts/main.js
+++ 01_dotinstall_flowershop/scripts/main.js
@@ -1,22 +1,52 @@
 'use strict';
-
+// 
 // popup products section
 function popup_products(el) {
-  function popup_appear(target) {
+  function popup_init(target) {
     // create new div element for popup style
     let newDiv = document.createElement("div");
     newDiv.className = "popup";
-    document.querySelector('body').appendChild(newDiv);
+    //
+    // Mask area
+    let newMask = document.createElement("div")
+    newMask.className = "mask hidden";
+    // Modal area
+    let newModal = document.createElement("section");
+    newModal.className = "modal hidden";
+    let cloneElement = target.cloneNode(true);
+    newModal.appendChild(cloneElement.querySelector("img"));
+    newModal.appendChild(cloneElement.querySelector("h2"));
+    newModal.appendChild(cloneElement.querySelector("p"));
+    // close button
+    let newClose = document.createElement("div");
+    newClose.className = "close";
+    newClose.textContent = "閉じる";
+    newModal.appendChild(newClose);
+    // append new element for popup product
+    newDiv.appendChild(newMask);
+    newDiv.appendChild(newModal);
     // target.appendChild(newDiv);
+    document.querySelector('body').appendChild(newDiv);
+    // add EventListner
+    newClose.addEventListener('click', () => {
+      // console.log('close clicked');
+      newModal.classList.add('hidden');
+      newMask.classList.add('hidden');
+    });
+    newMask.addEventListener('click', () => {
+      // ↓　2度書きを避けるため、close要素のclickメソッドを読み出す
+      newClose.click();
+    });
+    target.addEventListener('click', () => {
+      newModal.classList.remove('hidden');
+      newMask.classList.remove('hidden');
+    });
   }
   const targetList = document.querySelectorAll(el);
   targetList.forEach((target) => {
-    target.addEventListener('click', function() {
-      popup_appear(target);
-    });
+    // console.log(target);
+    popup_init(target);
   });
-
-
 }
 
 
@@ -24,5 +54,5 @@ function popup_products(el) {
 document.addEventListener('DOMContentLoaded', function () {
   hero_appear('.hero');
   isob_appear('.products li');
-  // popup_products('.products li');
+  popup_products('.products ul li');
 });
